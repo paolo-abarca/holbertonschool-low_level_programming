2d9dbc9764e042509e6cb62e57f07407c88d7878
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-03 16:16:58 -0800

    adding my first C program that prints text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-/*My first program with C language*/
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
