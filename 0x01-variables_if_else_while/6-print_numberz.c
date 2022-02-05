7a0ac7fb5a0cc35c912c2f4568c1b2b663976110
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-04 17:21:29 -0800

    adding script that prints numbers from 0 to 9 without char command

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+	putchar(ch);
+	}
+
+	putchar(10);
+
+	return (0);
+}
