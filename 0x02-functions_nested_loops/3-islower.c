df0c08594875162b6c27ccafb8914a6157db7344
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-08 18:20:16 -0800

    adding C program that prints if the letter c is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check if it is lowercase or not
+ * @c: charactec
+ * Return: 1 if c is lowercase
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
