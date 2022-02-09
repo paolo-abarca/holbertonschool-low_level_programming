f7c94352b952925c26f6d3c4923404172d5e1503
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-08 22:26:22 -0800

    adding C program that prints the entire table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table -
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int row;
+	int column;
+	int product;
+
+	for (row = 0; row <= 9; row++)
+	{
+	for (column = 0; column <= 9; column++)
+	{
+	product = (row * column);
+
+	if (column == 0)
+	{
+	_putchar(product + '0');
+	}
+	else if (product <= 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(product + '0');
+	}
+	else if (product > 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar((product / 10) + '0');
+	_putchar((product % 10) + '0');
+	}
+	}
+	_putchar('\n');
+	}
+}
