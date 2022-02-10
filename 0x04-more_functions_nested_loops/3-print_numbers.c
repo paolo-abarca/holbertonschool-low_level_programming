8d0d65719f610f2a5a4e480e7edd7ea6824b8620
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-10 14:47:45 -0800

    C project to print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers
+ *
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57 ; a++)
+	{
+	_putchar(a);
+	}
+	_putchar(10);
+}
