2e4385c17b5ec39be022e4c6445e8e5daa769cc1
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-10 14:14:19 -0800

    C project to multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+
+	return (r);
+}
