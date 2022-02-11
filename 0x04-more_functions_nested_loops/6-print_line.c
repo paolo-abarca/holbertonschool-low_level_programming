2c5a382f289279cc317472cec56c4542875fd7d1
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-10 17:16:22 -0800

    C project to print subscripts for the value of n

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints underscores depending on the value of n
+ * @n: the number of subscripts to print
+ * @a: the number of subscripts to print
+ *
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar(10);
+
+	}
+
+	else
+	{
+		int a;
+		for (a = 1; a <= n; a++)
+		{
+		_putchar ('_');
+		}
+		_putchar (10);
+	}
+}
