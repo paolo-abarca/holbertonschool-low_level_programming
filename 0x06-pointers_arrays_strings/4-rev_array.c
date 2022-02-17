6099543c1da01e6b5b70e6e8ec104da98194a9bc
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-17 15:53:14 -0800

    C function that prints the contents of an array in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - prototype
+ * @a: pointer
+ * @n: int
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, b, tmp;
+
+	b = n - 1;
+
+	for (i = 0; i < n / 2; i++)
+	{
+	tmp = a[i];
+	a[i] = a[b];
+	a[b--] = tmp;
+	}
+}
