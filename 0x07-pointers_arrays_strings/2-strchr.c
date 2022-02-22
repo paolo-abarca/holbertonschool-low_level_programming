124f4b177fd86471b8295b2d2a283ac26b622b9f
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-21 19:28:13 -0500

    adding C function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - prototype pointer
+ * @s: pointer
+ * @c: pointer
+ * Return: NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		return (s);
+		s++;
+	}
+
+	return ('\0');
+}
