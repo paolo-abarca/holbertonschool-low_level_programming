a6b0818d303f6335a80a3d953f8db28cd98891df
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-02-25 15:16:26 -0500

    C program to multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
