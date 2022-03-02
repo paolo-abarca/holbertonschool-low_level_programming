23fae851b7009b848555779733e2f929f8d96ae5
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-01 19:44:32 -0500

    adding C function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - function pointer
+ * @s1: pointer char
+ * @s2: pointer char
+ * Return: p
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int lens1, lens2;
+
+	for (lens1 = 0; s1[lens1]; lens1++)
+	{}
+	for (lens2 = 0; s2[lens2]; lens2++)
+	{}
+
+	p = malloc(sizeof(char) * (lens1 + lens2 + 1));
+
+	if (p != NULL)
+	{
+		for (lens1 = 0; s1[lens1]; lens1++)
+		{
+			p[lens1] = s1[lens1];
+		}
+		for (lens2 = 0 ; s2[lens2]; lens2++)
+		{
+			p[lens1 + lens2] = s2[lens2];
+		}
+		p[lens1 + lens2] = s2[lens2];
+	}
+
+	return (p);
+}
