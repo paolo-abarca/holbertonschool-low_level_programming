11706d3975f2b57be4cca4cd74e7b940e7ccdf9c
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-03 14:15:16 -0500

    adding C function that concatenates string s1 and the n bytes of string s2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - function pointer
+ * @s1: char pointer
+ * @s2: char pointer
+ * @n: unsigned int
+ * Return: p
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int lens1, lens2;
+
+	for (lens1 = 0; s1[lens1]; lens1++)
+	{}
+	for (lens2 = 0; s2[lens2]; lens2++)
+	{}
+
+	if (n >= lens2)
+		lens2 = lens2;
+	else
+	{
+		for (lens2 = 0; lens2 < n; lens2++)
+		{}
+	}
+
+	p = malloc(sizeof(char) * (lens1 + lens2 + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	else
+	{
+		for (lens1 = 0; s1[lens1]; lens1++)
+		{
+			p[lens1] = s1[lens1];
+		}
+		for (lens2 = 0 ; s2[lens2] != s2[n]; lens2++)
+		{
+			p[lens1 + lens2] = s2[lens2];
+		}
+		p[lens1 + lens2] = '\0';
+	}
+
+	return (p);
+}
