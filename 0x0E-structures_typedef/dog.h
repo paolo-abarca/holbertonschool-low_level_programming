ba51435a4a83905356c2c56b30d46a67ab0a3e23
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-07 20:51:51 -0500

    updating header file that defines a new type dog_t as a new name for the structure of type dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif
