bcfb82f96cf54df022c733d1d61c5db0fc40cbd2
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-09 16:05:19 -0500

    updating a C function that searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * int_index - function
@@ -11,6 +12,9 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
+	if (array == NULL || size <= 0 || cmp = NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
