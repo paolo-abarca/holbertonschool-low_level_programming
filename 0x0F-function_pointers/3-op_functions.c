8e9843c4dde5402d5f1f8de92797ee665a8d17dc
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-09 23:41:07 -0500

    adding C program containing the 5 main functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "calc.h"
+
+/**
+ * op_add - function sum
+ * @a: int
+ * @b: int
+ * Return: a+b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function subtraction
+ * @a: int
+ * @b: int
+ * Return: a-b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function multiplication
+ * @a: int
+ * @b: int
+ * Return: a*b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function division
+ * @a: int
+ * @b: int
+ * Return: a/b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function modulo
+ * @a: int
+ * @b: int
+ * Return: a%b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
