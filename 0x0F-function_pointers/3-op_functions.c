bb3cab4d574997f40be9eaf217ebb9c6215d26ec
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-09 20:57:04 -0800

    updating file

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,60 +1,59 @@
-#include "calc.h"
+#include "3-calc.h"
 
 /**
- * op_add - function sum
- * @a: int
- * @b: int
- * Return: a+b
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ * Return: an integer
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
-/**
- * op_sub - function subtraction
- * @a: int
- * @b: int
- * Return: a-b
- */
 
+/**
+ * op_sub - subtract
+ * @a: integer
+ * @b: integer
+ * Return: an integer
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
-/**
- * op_mul - function multiplication
- * @a: int
- * @b: int
- * Return: a*b
- */
 
+/**
+ * op_mul - multiply
+ * @a: integer
+ * @b: integer
+ * Return: an integer
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
-/**
- * op_div - function division
- * @a: int
- * @b: int
- * Return: a/b
- */
 
+/**
+ * op_div - divide
+ * @a: integer
+ * @b: integer
+ * Return: an integer
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
-/**
- * op_mod - function modulo
- * @a: int
- * @b: int
- * Return: a%b
- */
 
+/**
+ * op_mod - module
+ * @a: integer
+ * @b: integer
+ * Return: (a % b)
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
