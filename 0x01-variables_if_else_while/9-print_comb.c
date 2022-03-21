eb12d1f242905d05193a317033824cae8fa84b31
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-20 19:34:39 -0500

    adding C program that prints all possible combinations of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,15 @@ int main(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-	putchar(i);
-	if (i != 57)
+		putchar(i);
+
+		if (i != 57)
 		{
-		putchar(44);
-		putchar(32);
+			putchar(44);
+			putchar(32);
 		}
 	}
+
 	putchar(10);
 
 	return (0);
