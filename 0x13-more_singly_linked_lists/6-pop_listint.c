3c7b399a7bcf5c33cebf325a057bdff6d4aa33f3
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-22 11:55:23 -0500

    adding C function that removes the parent node from a linked list listint_t and returns the data of parent node (n)

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function int
+ *
+ * @head: double pointer
+ * Return: i
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *new_new;
+	int i;
+
+	if (head == NULL)
+		return (0);
+
+	i = (*head)->n;
+	new_new = (*head)->next;
+	free(*head);
+	*head = new_new;
+
+	return (i);
+}
