77ecf1c6510a4e1509cc3b02931c425c65fff8ac
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-05 18:35:05 -0500

    Addno C function that adds a new node to the beginning of a list dlistint_t

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function add double node int
+ *
+ * @head: double pointer
+ * @n: const in
+ * Return: new
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
