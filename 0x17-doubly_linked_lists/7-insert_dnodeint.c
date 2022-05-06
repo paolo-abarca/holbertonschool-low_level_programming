86992a1a3a4df24d63b3e82942550c0f759ed249
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-05 20:59:21 -0500

    Fixing a C function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,15 +26,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = *h;
-		*h = new;
-		return (new);
+		return (add_dnodeint(h, n));
 	}
 
 	for (x = 0; tmp != NULL && x < idx; x++)
 	{
 		if (x == idx - 1)
 		{
+			if (tmp->next == NULL)
+				return (add_dnodeint_end(h, n));
+
 			new->next = tmp->next;
 			new->prev = tmp;
 			tmp->next->prev = new;
