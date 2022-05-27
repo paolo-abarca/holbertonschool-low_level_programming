eb30aa17dcce9cbe59abcd1641ca39a6721f0326
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-27 12:56:18 -0500

    Updating C function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+
 	index = key_index((unsigned char *)key, ht->size);
 
 	node = malloc(sizeof(hash_node_t));
