c9867e9a452ce00663ea8d2f2a9d6ddce9d76200
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-27 10:53:46 -0500

    Fixing function Write a function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,8 +16,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = NULL;
+	table->array = malloc(sizeof(hash_node_t));
+	if (table->array == NULL)
+		return (NULL);
 
 	return (table);
-
 }
