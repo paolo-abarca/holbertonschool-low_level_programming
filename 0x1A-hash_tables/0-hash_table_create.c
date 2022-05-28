b16d8725e29ae4ebc876022797ffa173e6caed9d
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-27 21:04:28 -0500

    Updating function Write a function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 
 	return (table);
 }
