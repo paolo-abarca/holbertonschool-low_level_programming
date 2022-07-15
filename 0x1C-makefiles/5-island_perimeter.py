b532c2fa234e1d716adafa01e5348c8e1d0a0a59
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-07-14 22:02:54 -0500

    Fixing python function that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,23 +10,24 @@ def island_perimeter(grid):
     python function that returns the perimeter
     of the island described in grid
     """
+    group = 0
     perimeter = 0
     row = len(grid)
-    column = len(grid[0])
+    column = len(grid[group])
 
     for group in range(row):
         for cell in range(column):
             if grid[group][cell] == 1:
-                if grid[group - 1][cell] == 0 or group == 0:
+                if group == 0 or grid[group - 1][cell] == 0:
                     perimeter += 1
 
-                if grid[group + 1][cell] == 0 or group == row - 1:
+                if group == row - 1 or grid[group + 1][cell] == 0:
                     perimeter += 1
 
-                if grid[group][cell - 1] == 0 or cell == 0:
+                if cell == 0 or grid[group][cell - 1] == 0:
                     perimeter += 1
 
-                if grid[group][cell + 1] == 0 or cell == column - 1:
+                if cell == column - 1 or grid[group][cell + 1] == 0:
                     perimeter += 1
 
     return perimeter
